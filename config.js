bf379666c255041eb583506af796415782f4c1ce
Ritikasingh1003 <98206412+Ritikasingh1003@users.noreply.github.com>
2023-02-06 12:19:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    JWT_SECRET: "j1kqv/XWSmmUXTnmeyJ7KOqN9o3QtssRA5248y2C1K8=",
+    EMAIL: "edd60@ethereal.email", //testing email and password
+    PASSWORD: "rwGBs4eA8mvgpS324b"
+}
